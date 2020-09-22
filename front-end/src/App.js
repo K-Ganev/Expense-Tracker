8a04ec644811ac106851311d44e71e6df689c1ec
Kaloyan Ganev <k.ganev16@gmail.com>
2020-09-22 15:09:46 +0300

    Expense tracker front-end is commited

---
+++ front-end/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { GlobalProvider } from "./contexts/GlobalState";
+import { Header } from "./components/Header/Header";
+import { Balance } from "./components/Balance/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList/TransactionList";
+import { AddTransaction } from "./components/AddTransaction/AddTransaction";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
